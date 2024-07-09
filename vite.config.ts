3ec22325c52c44f4a93d34b86c9ab25c5a4193dc
phyuna0525 <phyunaa0525@gmail.com>
2024-07-09 11:02:49 +0900

    setting : setting

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import * as path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "./",
+  plugins: [react()],
+  assetsInclude: ["**/*.jpg"],
+  server: {
+    port: 4000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  define: {
+    "process.env": {},
+  },
+});
