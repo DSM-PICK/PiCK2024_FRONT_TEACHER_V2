27db327d3c35fc7c70aa0f5d97fe49c87c89318a
hyuna <hyuna_0525@dsm.hs.kr>
2024-07-10 09:28:08 +0900

    add : 로그인 페이지 및 메인 페이지

---
+++ src/apis/meal/index.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { instance } from "..";
+import { getFullToday } from "@/utils/date";
+import { TodayMealsType } from "./type";
+
+const router = "/meal";
+
+export const TodayMeals = () => {
+  return useQuery<TodayMealsType>({
+    queryKey: ["todayMeals"],
+    queryFn: async () => {
+      const { data } = await instance.get(
+        `${router}/date?date=${getFullToday()}`
+      );
+      return data;
+    },
+  });
+};
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,124 @@
+import { useLogin } from "@/apis/admin";
+import Button from "@/components/button";
+import Input from "@/components/input";
+import { theme } from "@/styles/theme";
+import { saveToken } from "@/utils/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
+
+const Login = () => {
+  const [adminId, setAdminId] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const { mutate: login } = useLogin();
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleAdminIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAdminId(e.target.value);
+  };
+
+  const handleKeyDown = async (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === "Enter") {
+      SubmitLogin();
+    }
+  };
+
+  const router = useNavigate();
+
+  const SubmitLogin = () => {
+    login(
+      { admin_id: adminId, password: password },
+      {
+        onSuccess: (res) => {
+          const accessToken = res.access_token;
+          const refreshToken = res.refresh_token;
+          router("/main");
+          saveToken(accessToken, refreshToken);
+        },
+      }
+    );
+  };
+
+  return (
+    <Container>
+      <ContentWrap>
+        <TextWrap>
+          <LoginTitle>
+            <HilightText>PiCK</HilightText>에 로그인하기
+          </LoginTitle>
+          <LoginSubTitle>스퀘어 계정으로 로그인 해주세요.</LoginSubTitle>
+        </TextWrap>
+        <InputWrap>
+          <Input
+            type="text"
+            label="아이디"
+            placeholder="아이디를 입력해주세요"
+            onChange={handleAdminIdChange}
+            name="admin_id"
+            value={adminId}
+          />
+          <Input
+            type="password"
+            label="비밀번호"
+            placeholder="비밀번호를 입력해주세요"
+            onChange={handlePasswordChange}
+            name="password"
+            value={password}
+            onKeyDown={handleKeyDown}
+          />
+        </InputWrap>
+      </ContentWrap>
+      <Button width="100%" onClick={SubmitLogin}>
+        로그인하기
+      </Button>
+    </Container>
+  );
+};
+
+export default Login;
+
+const LoginTitle = styled.p`
+  font-size: ${theme.font.heading[2].size};
+  font-weight: ${theme.font.heading[2].fontweight};
+`;
+
+const HilightText = styled.span`
+  color: ${theme.color.main[500]};
+`;
+
+const LoginSubTitle = styled.p`
+  font-size: ${theme.font.body[1].size};
+  font-weight: ${theme.font.body[1].fontweight};
+  color: ${theme.color.gray[600]};
+`;
+
+const TextWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const Container = styled.div`
+  padding: 60px 24px;
+  height: 100dvh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const ContentWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
+`;
+
+const InputWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
