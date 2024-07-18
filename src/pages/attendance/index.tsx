1518dc14448b306a1165325dfc11f7531a49c599
hyuna <hyuna_0525@dsm.hs.kr>
2024-07-18 20:03:19 +0900

    add :: 자습 출결 관리

---
+++ src/pages/attendance/index.tsx
@@ -0,0 +1,67 @@
+import { AttendanceCheck } from "@/apis/attendance";
+import { AttendType } from "@/apis/attendance/type";
+import AttendanceList from "@/components/attendList";
+import Dropdown from "@/components/dropdown/dropdown";
+import Layout from "@/components/layout/layout";
+import Tab from "@/components/tab/tab";
+import { getStudentString } from "@/utils/util";
+import { useEffect, useState } from "react";
+
+const Attendance = () => {
+  const tab = ["8교시", "9교시", "10교시"];
+  const [selectedTab, setSelectedTab] = useState<number>(0);
+  const [selectedGrade, setSelectedGrade] = useState<number>(1);
+  const [selectedClass, setSelectedClass] = useState<number>(1);
+  const [data, setData] = useState<AttendType[]>([]);
+
+  const { mutate: GetCheckList } = AttendanceCheck();
+
+  const Get = () => {
+    GetCheckList(
+      { grade: selectedGrade, class_num: selectedClass },
+      {
+        onSuccess: (data) => {
+          setData(data);
+        },
+      }
+    );
+  };
+
+  const handleTabClick = (index: number) => {
+    setSelectedTab(index);
+  };
+
+  const handleGradeChange = (option: number) => {
+    setSelectedGrade(option);
+  };
+
+  const handleClassChange = (option: number) => {
+    setSelectedClass(option);
+  };
+
+  useEffect(() => {
+    Get();
+  }, [selectedGrade, selectedClass]);
+
+  return (
+    <Layout
+      title="출석 체크"
+      right={
+        <>
+          <Dropdown type="grade" onChange={handleGradeChange} />
+          <Dropdown type="class" onChange={handleClassChange} />
+        </>
+      }
+    >
+      <Tab content={tab} onClick={handleTabClick} selectedIndex={selectedTab} />
+      {data.map((item) => (
+        <AttendanceList
+          userInfo={getStudentString(item)}
+          status={item.status6}
+        />
+      ))}
+    </Layout>
+  );
+};
+
+export default Attendance;
