d9a40b44ffaeedde22eaa27b573ac10a5e538352
hyuna <hyuna_0525@dsm.hs.kr>
2024-08-01 16:54:40 +0900

    CHORE :: 드롭다운 리팩토링

---
+++ src/types/dropdown.ts
@@ -0,0 +1,69 @@
+export type Option = {
+  label: string | number;
+  value: number;
+};
+
+export const floorOptions = [
+  { value: 2, label: "2층" },
+  { value: 3, label: "3층" },
+  { value: 4, label: "4층" },
+  { value: 5, label: "전체" },
+];
+
+export const AllOption = [
+  { value: 1, label: "1학년" },
+  { value: 2, label: "2학년" },
+  { value: 3, label: "3학년" },
+  { value: 5, label: "전체" },
+];
+
+export const gradeOptions = [
+  { value: 1, label: "1학년" },
+  { value: 2, label: "2학년" },
+  { value: 3, label: "3학년" },
+];
+
+export const AllclassOptions = [
+  { value: 1, label: "1반" },
+  { value: 2, label: "2반" },
+  { value: 3, label: "3반" },
+  { value: 4, label: "4반" },
+  { value: 5, label: "전체" },
+];
+
+export const classOptions = [
+  { value: 1, label: "1반" },
+  { value: 2, label: "2반" },
+  { value: 3, label: "3반" },
+  { value: 4, label: "4반" },
+];
+
+export const clubOptions = [
+  { value: "자습", label: "3-1교실(자습)" },
+  { value: "대동여지도", label: "세미나실 2-1(대동여지도)" },
+  { value: "DMS", label: "세미나실 2-2(DMS)" },
+  { value: "gram", label: "세미나실 2-3(gram)" },
+  { value: "Liear", label: "세미나실 2-4(Liear)" },
+  { value: "gram1", label: "3-2교실(gram)" },
+  { value: "EXIT", label: "소개1실(EXIT)" },
+  { value: "Lift", label: "소개2실(Lift)" },
+  { value: "DMS3학년", label: "소개 3실(DMS 3학년)" },
+  { value: "자습", label: "2-1교실(자습)" },
+  { value: "Log", label: "세미나실 3-1(Log)" },
+  { value: "은하", label: "세미나실 3-2(은하)" },
+  { value: "PiCK", label: "세미나실 3-3(PiCK)" },
+  { value: "어게인", label: "보안 1실(어게인)" },
+  { value: "info", label: "보안 2실(info)" },
+  { value: "TeamQSS", label: "세미나실 4-1(TeamQSS)" },
+  { value: "NoNamed", label: "세미나실 4-2(NoNamed)" },
+  { value: "Modeep", label: "세미나실 4-3(Modeep)" },
+  { value: "자습", label: "1-1교실(자습)" },
+];
+
+export const classTimeOption = [
+  { value: 6, label: "6교시" },
+  { value: 7, label: "7교시" },
+  { value: 8, label: "8교시" },
+  { value: 9, label: "9교시" },
+  { value: 10, label: "10교시" },
+];
