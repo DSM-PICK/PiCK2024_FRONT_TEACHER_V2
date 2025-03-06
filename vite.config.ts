e2990c390c097da92b0b882889526ed493b7c494
HYUNA <phyunaa0525@gmail.com>
2025-03-06 23:02:23 +0900

    add :: allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import * as path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
 import envCompatiblePlugin from 'vite-plugin-env-compatible'
 
 
 export default defineConfig({
   base: "./",
-  plugins: [react(), svgr(), envCompatiblePlugin()],
+  plugins: [react(), envCompatiblePlugin()],
   assetsInclude: ["**/*.jpg"],
   server: {
+    host: "0.0.0.0",
     port: 3000,
+    allowedHosts: [
+      "pick-admin-stag.xquare.app",
+      "pick-admin.xquare.app",
+    ],
   },
   resolve: {
     alias: [
