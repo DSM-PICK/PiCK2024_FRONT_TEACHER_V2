77f5af037e15d273c5495a34db70d74ef3b61ab3
1408bg <bgyang1220@gmail.com>
2025-10-19 22:37:30 +0900

    fix :: pwa 미적용 수정

--- vite.config.ts
+++ vite.config.ts
@@ -25,12 +25,12 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: "logo192.png",
+            src: "icon-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "logo512.png",
+            src: "icon-512.png",
             sizes: "512x512",
             type: "image/png",
           },
