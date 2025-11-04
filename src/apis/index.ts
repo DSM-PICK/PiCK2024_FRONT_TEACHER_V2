bd34e7f9e415ed520cf0e053ea564bc9c213c59e
kimsiwoo <ksw98035213@gmail.com>
2025-11-04 20:18:42 +0900

    fix :: 리프레쉬 토큰 로직 수정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -8,11 +8,6 @@ export const instance = axios.create({
   timeout: 10000,
 });
 
-export const refreshInstance = axios.create({
-  baseURL: BASEURL,
-  timeout: 10000,
-});
-
 instance.interceptors.request.use(
   (config) => {
     if (typeof window !== "undefined") {
@@ -24,39 +19,35 @@ instance.interceptors.request.use(
   (error: AxiosError) => Promise.reject(error)
 );
 
-refreshInstance.interceptors.request.use(
-  (config) => {
-    const refreshToken = cookie.get("refresh_token");
-    if (refreshToken) {
-      config.headers.Authorization = `Bearer ${refreshToken}`;
-    }
-    return config;
-  },
-  (error: AxiosError) => Promise.reject(error)
-);
 instance.interceptors.response.use(
   (response) => response,
   async (error: AxiosError) => {
-    if (axios.isAxiosError(error) && error.response) {
-      const { status } = error.response;
-      if (status === 401) {
+    if (axios.isAxiosError(error)) {
+      const status = error.code;
+      const originalRequest = error.config as typeof error.config & {
+        _retry?: boolean;
+      };
+
+      if (status === "ERR_NETWORK" && !originalRequest._retry) {
+        originalRequest._retry = true;
+
         const refreshToken = cookie.get("refresh_token");
         try {
-          await axios
-            .put(`${BASEURL}/admin/refresh`, null, {
-              headers: {
-                "X-Refresh-Token": `${refreshToken}`,
-              },
-            })
-            .then((response) => {
-              const data = response.data;
-              cookie.set("access_token", data.access_token);
-              cookie.set("refresh_token", data.refresh_token);
-            })
-            .catch(() => {
-              window.location.href = "/";
-            });
+          const response = await axios.put(`${BASEURL}/admin/refresh`, null, {
+            headers: {
+              "X-Refresh-Token": `${refreshToken}`,
+            },
+          });
+          const data = response.data;
+          cookie.set("access_token", data.access_token);
+          cookie.set("refresh_token", data.refresh_token);
+
+          if (originalRequest) {
+            originalRequest.headers.Authorization = `Bearer ${data.access_token}`;
+            return instance(originalRequest);
+          }
         } catch (refreshError) {
+          window.location.href = "/";
           return Promise.reject(refreshError);
         }
       }
