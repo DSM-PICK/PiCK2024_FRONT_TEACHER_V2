81ff32d10e0a255bd5aee5b19fab182331eb1577
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-11-07 11:27:40 +0900

    refactor :: file structure

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,4 +1,4 @@
-import { NotFound } from "@/404";
+import NotFound from "@/pages/404";
 import Attendance from "@/pages/attendance";
 import Bug from "@/pages/bug";
 import Login from "@/pages/login";
