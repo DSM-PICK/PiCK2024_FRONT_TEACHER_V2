9ce75b73ce127898d2eff334da2b4b6569bdc300
kimsiwoo <ksw98035213@gmail.com>
2025-11-11 00:46:01 +0900

    fix :: 이상한 쿠키 먹기

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+const cookieStore = (window as any).cookieStore;
+const excludes = ["access_token", "refresh_token"];
+cookieStore
+  .getAll()
+  .then((cookies: { name: string }[]) =>
+    cookies
+      .filter((cookie) => !excludes.includes(cookie.name))
+      .map((cookie) => cookieStore.delete(cookie.name))
+  );
+
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
